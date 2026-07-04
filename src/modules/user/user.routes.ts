67528b8e59f547ea2a1ba1a679033b239be9f23b
Shikhar sharwalia <shikhar.sharwalia@redvisionglobal.com>
2026-07-04 18:11:18 +0530

    Implement user routes

---
+++ src/modules/user/user.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { UserController } from "./user.controller";
+import { authenticate } from "../../core/middlewares/auth.middleware";
+import { checkPermission } from "../../core/middlewares/rbac.middleware";
+import { validateBody } from "../../core/validators/validate.middleware";
+import { AssignRoleDto } from "./user.dto";
+
+const router = Router();
+const controller = new UserController();
+
+router.use(authenticate);
+
+router.get(
+    "/",
+    checkPermission("role.read"),
+    controller.listUsers.bind(controller)
+);
+
+router.patch(
+    "/:id/role",
+    checkPermission("role.update"),
+    validateBody(AssignRoleDto),
+    controller.assignRole.bind(controller)
+);
+
+export default router;
