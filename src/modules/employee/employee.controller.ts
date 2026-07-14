72aa5e7de0113e1c308f62b7c1281ad1565a1275
Shikhar sharwalia <shikhar.sharwalia@redvisionglobal.com>
2026-07-14 12:37:56 +0530

    update the employee controller adding document view and upload url controller

--- src/modules/employee/employee.controller.ts
+++ src/modules/employee/employee.controller.ts
@@ -257,4 +257,37 @@ export class EmployeeController {
       next(error);
     }
   }
+
+  // documents 
+
+  async requestUploadUrl(
+  req: Request<{ id: string }>, 
+  res: Response, 
+  next: NextFunction
+  ): Promise<void> {
+  try {
+    const result = await empService.requestDocumentUploadUrl(req.context, req.params.id, req.body);
+    res.status(200).json(buildSuccessResponse(result, "Upload URL generated"));
+  } catch (error) {
+     next(error); 
+    }
 }
+
+
+async getDownloadUrl(
+  req: Request<{ id: string; docId: string }>, 
+  res: Response, 
+  next: NextFunction
+  ): Promise<void> {
+  try {
+    const result = await empService.getDocumentDownloadUrl(req.context, req.params.id, req.params.docId);
+    res.status(200).json(buildSuccessResponse(result, "Download URL generated"));
+  } catch (error) { 
+    next(error); 
+  }
+}
+
+}
+
+
+
