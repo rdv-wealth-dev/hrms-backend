fda093650a425ad3b296a706e419a4f1edc99d93
Shikhar sharwalia <shikhar.sharwalia@redvisionglobal.com>
2026-08-03 11:16:53 +0530

    implement audit routes for logs

---
+++ src/modules/audit/audit.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { AuditController } from "./audit.controller";
+import { authenticate } from "../../core/middlewares/auth.middleware";
+import { checkPermission } from "../../core/middlewares/rbac.middleware";
+
+const router = Router();
+const ctrl = new AuditController();
+router.use(authenticate);
+
+router.get(
+    "/sessions",
+    checkPermission("role.read"),
+    ctrl.getSessions.bind(ctrl)
+);
+
+router.get(
+    "/actions",
+    checkPermission("role.read"),
+    ctrl.getActions.bind(ctrl)
+);
+
+export default router;
