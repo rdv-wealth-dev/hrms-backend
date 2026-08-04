2b1ddecbf81ca33b413373c06ba303314860c7c8
Shikhar sharwalia <shikhar.sharwalia@redvisionglobal.com>
2026-08-04 11:16:40 +0530

    these plugins are link with india plugin because we need to create the branches if existin AE and US

--- src/countries/index.ts
+++ src/countries/index.ts
@@ -1,6 +1,10 @@
 import { CountryRegistry } from "../core/plugins/country-registry";
 import { IndiaPlugin } from "./IN";
+import { USAPlugin } from "./US";
+import { UAEPlugin } from "./AE";
 
 export function initializeCountryPlugins(): void {
   CountryRegistry.register(IndiaPlugin);
+  CountryRegistry.register(USAPlugin);
+  CountryRegistry.register(UAEPlugin);
 }
